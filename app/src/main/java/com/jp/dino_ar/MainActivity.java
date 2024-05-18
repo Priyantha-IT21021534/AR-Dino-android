9aa5c585f2e6f0fa39058af185a260482c9d575b
Priyantha <IT21021534@my.sliit.lk>
2024-05-18 14:49:51 +0530

    load from local assets

--- app/src/main/java/com/jp/dino_ar/MainActivity.java
+++ app/src/main/java/com/jp/dino_ar/MainActivity.java
@@ -1,13 +1,8 @@
 package com.jp.dino_ar;
 
 import android.Manifest;
-import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.util.Log;
 import android.webkit.ConsoleMessage;
 import android.webkit.PermissionRequest;
@@ -28,9 +23,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
   private static final int PERMISSIONS_REQUEST_CODE = 100;
   private static final String[] PERMISSIONS = {
-      Manifest.permission.CAMERA,
-      Manifest.permission.INTERNET,
-      Manifest.permission.ACCESS_NETWORK_STATE
+      Manifest.permission.CAMERA
   };
   private WebView webView;
 
@@ -52,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
     webSettings.setMediaPlaybackRequiresUserGesture(false);
     webSettings.setPluginState(WebSettings.PluginState.ON);
     webSettings.setBuiltInZoomControls(true);
+    webSettings.setAllowFileAccessFromFileURLs(true);
 
     webView.setWebChromeClient(new WebChromeClient() {
       @Override
@@ -71,11 +65,7 @@ public class MainActivity extends AppCompatActivity {
     webView.setWebViewClient(new WebViewClientHandler());
 
     if (checkPermissions()) {
-      if (isInternetAvailable()) {
-        loadWebView();
-      } else {
-        showNoInternetMessage();
-      }
+      loadWebView();
     } else {
       requestPermissions();
     }
@@ -94,20 +84,8 @@ public class MainActivity extends AppCompatActivity {
     requestPermissions(PERMISSIONS, PERMISSIONS_REQUEST_CODE);
   }
 
-  private boolean isInternetAvailable() {
-    ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-    NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-    return activeNetwork != null && activeNetwork.isConnected();
-  }
-
-  private void showNoInternetMessage() {
-    Toast.makeText(this, "No internet connection available", Toast.LENGTH_LONG).show();
-    // Optionally, navigate to device settings to enable internet
-    startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
-  }
-
   private void loadWebView() {
-    webView.loadUrl("https://priyantha-it21021534.github.io/ctse-ar-dinowrhcbb/");
+    webView.loadUrl("file:///android_asset/index.html");
   }
 
   @Override
@@ -115,14 +93,9 @@ public class MainActivity extends AppCompatActivity {
     super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     if (requestCode == PERMISSIONS_REQUEST_CODE) {
       if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-        if (isInternetAvailable()) {
-          loadWebView();
-        } else {
-          showNoInternetMessage();
-        }
+        loadWebView();
       } else {
         Toast.makeText(this, "Permissions are required for the app to function", Toast.LENGTH_LONG).show();
-        // Optionally, close the app or navigate to settings
       }
     }
   }
@@ -136,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
     }
   }
 
-  private class WebViewClientHandler extends WebViewClient {
+  private static class WebViewClientHandler extends WebViewClient {
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
       return false;
